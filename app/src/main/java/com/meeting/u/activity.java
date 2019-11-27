b024d50aab0d610f8b6bf5f05c2e22de4b2f7445
sebastian <jupachonc@unal.edu.co>
2019-11-27 14:22:59 -0500

    activity constructor

--- app/src/main/java/com/meeting/u/activity.java
+++ app/src/main/java/com/meeting/u/activity.java
@@ -7,4 +7,16 @@ public class activity {
     String hora_incio;
     String hora_fin;
     String[] participantes;
+
+    activity(String tipo, String descripción, String hora_incio, String hora_fin, int participantes){
+
+        this.tipo = tipo;
+        this.descripción = descripción;
+        this.hora_incio = hora_incio;
+        this.hora_fin = hora_fin;
+        this.participantes = new String[participantes];
+
+    }
+
+
 }
