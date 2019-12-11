3c7da56b8f1af3bb23d682c8a20e11eabf6ceaae
JuanMora2017 <juan120101@hotmail.com>
2019-12-10 19:33:35 -0500

    Superficial Things

---
+++ app/src/main/java/com/meeting/u/RamdomActivity.java
@@ -0,0 +1,14 @@
+package com.meeting.u;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RamdomActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ramdom);
+    }
+}
